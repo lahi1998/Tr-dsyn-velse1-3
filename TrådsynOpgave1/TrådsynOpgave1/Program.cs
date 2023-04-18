86988959f344a08f3a4f9efa7de05ded8a8b0e79
lahi1998 <95356006+lahi1998@users.noreply.github.com>
2023-04-18 09:57:03 +0200

    Add files via upload

---
+++ TrådsynOpgave1/TrådsynOpgave1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+namespace TrådsynOpgave1
+{
+    class Program
+    {
+        static int Count;
+        static readonly object  Lock = new object();
+
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run == true)
+            {
+
+                    Thread t1 = new Thread(CountUp);
+                t1.Name = "CountUp";
+                t1.Priority = ThreadPriority.Normal;
+                t1.Start();
+
+                Thread t2 = new Thread(CountDown);
+                t2.Name = "CountDown";
+                t2.Priority = ThreadPriority.Normal;
+                t2.Start();
+
+            }
+        }
+
+        public static void CountUp()
+        {
+            Monitor.Enter(Lock);
+            try
+            {
+
+                    Count++;
+                    Count++;
+                    Console.WriteLine(Count);
+                    Thread.Sleep(1000);
+                
+            }
+            finally
+            {
+                Monitor.Exit(Lock);
+            }
+        }
+
+        public static void CountDown()
+        {
+            Monitor.Enter(Lock);
+            try
+            {
+
+                    Count--;
+                    Console.WriteLine(Count);
+                    Thread.Sleep(1000);
+                
+
+            }
+            finally
+            {
+                Monitor.Exit(Lock);
+            }
+        }
+    }
+}
---
+++ TrådsynOpgave2-3/TrådsynOpgave2-3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace TrådsynOpgave2_3
+{
+    class Program
+    {
+        static int Count;
+        static readonly object Lock = new object();
+
+        static void Main(string[] args)
+        {
+            bool run = true;
+            while (run == true)
+            {
+
+                Thread t1 = new Thread(Star);
+                t1.Name = "*";
+                t1.Priority = ThreadPriority.Normal;
+                t1.Start();
+
+                Thread t2 = new Thread(Pound);
+                t2.Name = "#";
+                t2.Priority = ThreadPriority.Normal;
+                t2.Start();
+
+            }
+        }
+
+        public static void Star()
+        {
+            Monitor.Enter(Lock);
+            try
+            {
+                Console.WriteLine("************************************************************");
+                Thread.Sleep(50);
+            }
+            finally
+            {
+                Monitor.Exit(Lock);
+            }
+        }
+
+        public static void Pound()
+        {
+            Monitor.Enter(Lock);
+            try
+            {
+                Console.WriteLine("############################################################");
+                Thread.Sleep(50);
+            }
+            finally
+            {
+                Monitor.Exit(Lock);
+            }
+        }
+    }
+}
